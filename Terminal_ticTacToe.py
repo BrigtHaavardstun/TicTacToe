2c7c967f28a5d98e07894dc66c75d8f8f28c7f1d
Brigt Arve Toppe Håvardstun <Brigt_arve@outlook.com>
2019-09-11 12:53:52 +0200

    Visual improvment

--- Terminal_ticTacToe.py
+++ Terminal_ticTacToe.py
@@ -4,7 +4,7 @@ def got_draw(board):
     draw = True
     for row in board:
         for cell in row:
-            if cell == "":
+            if cell == " ":
                 draw = False
     
     if draw:
@@ -14,22 +14,22 @@ def got_winner(board):
 
     # Vertical
     for y in range(3):
-        if board[0][y] == board[1][y] and board[1][y] == board[2][y] and board[0][y] != "":
+        if board[0][y] == board[1][y] and board[1][y] == board[2][y] and board[0][y] != " ":
             print("we have a winner!")
             return True
     #  Horizaontal
     for x in range(3):
-        if board[x][0] == board[x][1] and board[x][1] == board[x][2] and board[x][0] != "":
+        if board[x][0] == board[x][1] and board[x][1] == board[x][2] and board[x][0] != " ":
             print("we have a winner!")
             return True
 
     # Top left to bottom right 
-    if board[0][0] == board[1][1] and board[1][1] == board[2][2] and board[1][1] != "":
+    if board[0][0] == board[1][1] and board[1][1] == board[2][2] and board[1][1] != " ":
         print("we have a winner!")
         return True
 
     # Top right to bottom left
-    if board[2][0] == board[1][1] and board[1][1] == board[0][2] and board[1][1] != "":
+    if board[2][0] == board[1][1] and board[1][1] == board[0][2] and board[1][1] != " ":
         print("we have a winner!")
         return True
 
@@ -37,25 +37,22 @@ def got_winner(board):
     return False
 
 
-
-board = []
-
 w,h = 3,3
 # set board
-board = [["" for x in range(w) ] for y in range(h) ]
-print(board)
+board = [[" " for x in range(w) ] for y in range(h) ]
 
 
 # X-es start
 player = "X"
 
 # Print the board
-for row in board:
-    print(row)
+print("    0    1    2")
+for i in range(len(board)-1, -1, -1):
+    print(str(i) + " " + str(board[i]))
 
 while not got_winner(board) and not got_draw(board):
     print(f"It's {player}s turn!")
-    target = input("Where do you want to place your pice? give input like '3 1': ")
+    target = input("Where do you want to place your pice? give input like '1 1': ")
     x_pos, y_pos = target.split()
 
     # Convert the input text to numbers
@@ -63,7 +60,7 @@ while not got_winner(board) and not got_draw(board):
     y_pos = int(y_pos)
 
 
-    if board[y_pos][x_pos] != "":
+    if board[y_pos][x_pos] != " ":
         print("That tile is taken, try agian")
     else:
         board[y_pos][x_pos] = player
@@ -74,6 +71,8 @@ while not got_winner(board) and not got_draw(board):
         else:
             player = "X"
 
+
         # Print the board
-        for row in board:
-            print(row)
+        print("    0    1    2")
+        for i in range(len(board)-1, -1, -1):
+            print(str(i) + " " + str(board[i]))
